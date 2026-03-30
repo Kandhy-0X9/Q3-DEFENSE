06c15c46e9019f8f845bba9686412d668de35f68
Kandhy-0X9 <kashengkandhy@gmail.com>
2026-03-30 13:41:38 -0400

    starting

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-##
+# Turtle Racing game
+from turtle import *
+import time
+import random
+
+# set up
+screen = Screen()
+screen.setup(width=500, height=500)
+title("Turtle Race")
+bgcolor('forestgreen')
+speed('fastest')
